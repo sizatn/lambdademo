38cde646b2572a9991130ffd88d522443797bebf
sizatn <sizatn2013@gmail.com>
2017-12-31 15:43:42 +0800

    update

---
+++ src/com/sizatn/lambda/stream/prime/Prime.java
@@ -0,0 +1,76 @@
+package com.sizatn.lambda.stream.prime;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 
+ * @desc 将数字按质数和非质数分区
+ * @author sizatn
+ * @date Dec 25, 2017
+ */
+public class Prime {
+
+	public static void main(String[] args) {
+		Map<Boolean, List<Integer>> primesList1 = partitionPrimes(20);
+		primesList1.forEach((key, value) -> {
+			System.out.println(key.toString() + " = " + value.toString());
+		});
+
+		System.out.println("");
+
+		Map<Boolean, List<Integer>> primesList2 = partitionPrimesWithCustomCollector(20);
+		primesList2.forEach((key, value) -> {
+			System.out.println(key.toString() + " = " + value.toString());
+		});
+
+		System.out.println("");
+
+	}
+
+	/**
+	 * @param n
+	 * @return
+	 * @desc partitioningBy实现质数
+	 * @author sizatn
+	 * @date Dec 31, 2017
+	 */
+	private static Map<Boolean, List<Integer>> partitionPrimes(int n) {
+		return IntStream.rangeClosed(2, n).boxed().collect(Collectors.partitioningBy(candidate -> isPrime(candidate)));
+	}
+
+	private static boolean isPrime(int candidate) {
+		int candidateRoot = (int) Math.sqrt((double) candidate);
+		return IntStream.rangeClosed(2, candidateRoot).noneMatch(i -> candidate * i == 0);
+	}
+
+	/**
+	 * @param n
+	 * @return
+	 * @desc 自定义收集器实现质数
+	 * @author sizatn
+	 * @date Dec 31, 2017
+	 */
+	private static Map<Boolean, List<Integer>> partitionPrimesWithCustomCollector(int n) {
+		return IntStream.rangeClosed(2, n).boxed().collect(new PrimeNumbersCollector());
+	}
+
+	@SuppressWarnings("serial")
+	private static Map<Boolean, List<Integer>> partitionPrimesWithCustomCollector1(int n) {
+		return IntStream.rangeClosed(2, n).boxed().collect(() -> new HashMap<Boolean, List<Integer>>() {
+			{
+				put(true, new ArrayList<>());
+				put(false, new ArrayList<>());
+			}
+		}, (m, i) -> {
+			m.get(PrimeNumbersCollector.isPrime(m.get(true), i)).add(i);
+		}, (m1, m2) -> {
+			m1.get(true).addAll(m2.get(true));
+			m1.get(false).addAll(m2.get(false));
+		});
+	}
+}
--- src/com/sizatn/lambda/stream/prime/PrimeNumbersCollector.java
+++ src/com/sizatn/lambda/stream/prime/PrimeNumbersCollector.java
@@ -1,6 +1,8 @@
-package com.sizatn.lambda.stream;
+package com.sizatn.lambda.stream.prime;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,7 +16,10 @@ import java.util.stream.Collector;
 
 /**
  * 
- * @desc 自定义收集器
+ * @desc 自定义收集器，实现Collector接口，这里定义了一个质数（prime number）收集器
+ * @desc Integer：流中元素的类型
+ * @desc 第一个Map<Boolean, List<Integer>>：累加器类型
+ * @desc 第二个Map<Boolean, List<Integer>>：collect操作的结果类型
  * @author sizatn
  * @date Dec 27, 2017
  */
@@ -22,8 +27,8 @@ public class PrimeNumbersCollector
 		implements Collector<Integer, Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> {
 
 	/**
-	 * @return
-	 * @desc
+	 * @return 返回一个在调用时创建累加器的函数
+	 * @desc 创建用作累加器的Map，且初始化true和false两个键用来分别添加质数和非质数
 	 * @author sizatn
 	 * @date Dec 27, 2017
 	 */
@@ -40,7 +45,8 @@ public class PrimeNumbersCollector
 
 	/**
 	 * @return
-	 * @desc 
+	 * @desc 定义了如何收集流中元素的逻辑
+	 * @desc 根据isPrime的结果，获取质数或非质数列表，将当前被测数添加到相应的列表中
 	 * @author sizatn
 	 * @date Dec 27, 2017
 	 */
@@ -53,7 +59,7 @@ public class PrimeNumbersCollector
 
 	/**
 	 * @return
-	 * @desc 
+	 * @desc 把两个部分累加器合并起来
 	 * @author sizatn
 	 * @date Dec 27, 2017
 	 */
@@ -68,29 +74,27 @@ public class PrimeNumbersCollector
 
 	/**
 	 * @return
-	 * @desc 
+	 * @desc 将累加器转换为collect操作的结果类型，此处不用转换，因此用identity函数收尾
 	 * @author sizatn
 	 * @date Dec 27, 2017
 	 */
 	@Override
 	public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
-		// TODO Auto-generated method stub
-		return null;
+		return Function.identity();
 	}
 
 	/**
 	 * @return
-	 * @desc 
+	 * @desc 这个收集器是IDENTITY_FINISH，但既不是UNORDERED也不是CONCURRENT，因为质数是按顺序发现的
 	 * @author sizatn
 	 * @date Dec 27, 2017
 	 */
 	@Override
 	public Set<Characteristics> characteristics() {
-		// TODO Auto-generated method stub
-		return null;
+		return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH));
 	}
 
-	private static boolean isPrime(List<Integer> primes, int candidate) {
+	public static boolean isPrime(List<Integer> primes, int candidate) {
 		int candidateRoot = (int) Math.sqrt((double) candidate);
 		return takeWhile(primes, i -> i <= candidateRoot).stream().noneMatch(p -> candidate % p == 0);
 	}
